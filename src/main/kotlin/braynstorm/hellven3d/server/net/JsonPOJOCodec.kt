2c5839e1953e2673bcd2240a2edc3500daf8f6f9
braynstorm <glav0r3zzz4@gmail.com>
2017-08-30 07:39:56 +0300

    Added ability to use more than one POJO superclass.

--- src/main/kotlin/braynstorm/hellven3d/server/net/JsonPOJOCodec.kt
+++ src/main/kotlin/braynstorm/hellven3d/server/net/JsonPOJOCodec.kt
@@ -1,18 +1,17 @@
 package braynstorm.hellven3d.server.net
 
-import POJO
 import com.fasterxml.jackson.databind.ObjectMapper
 import io.netty.buffer.ByteBuf
 import io.netty.channel.ChannelHandlerContext
 import io.netty.handler.codec.MessageToMessageDecoder
 import io.netty.handler.codec.MessageToMessageEncoder
 
-class JsonPOJOEncoder : MessageToMessageEncoder<POJO>() {
+class JsonPOJOEncoder<T>(private val pojoClass: Class<T>) : MessageToMessageEncoder<T>() {
 	companion object {
 		val objectMapper = ObjectMapper()
 	}
 
-	override fun encode(ctx: ChannelHandlerContext, msg: POJO, out: MutableList<Any>) {
+	override fun encode(ctx: ChannelHandlerContext, msg: T, out: MutableList<Any>) {
 		// By specification, Charsets.UTF_8 is the charset
 		val bytes = objectMapper.writeValueAsBytes(msg)
 		val byteBuf = ctx.alloc().buffer(bytes.size).writeBytes(bytes)
@@ -20,7 +19,7 @@ class JsonPOJOEncoder : MessageToMessageEncoder<POJO>() {
 	}
 }
 
-class JsonPOJODecoder : MessageToMessageDecoder<ByteBuf>() {
+class JsonPOJODecoder<T>(private val pojoClass: Class<T>) : MessageToMessageDecoder<ByteBuf>() {
 	companion object {
 		val objectMapper = ObjectMapper()
 	}
@@ -29,8 +28,11 @@ class JsonPOJODecoder : MessageToMessageDecoder<ByteBuf>() {
 		// By specification, Charsets.UTF_8 is the charset
 		val jsonString = msg.toString(Charsets.UTF_8)
 		try {
-			val pojo = objectMapper.readValue<POJO>(jsonString, POJO::class.java)
-			out += pojo
+			val pojo = objectMapper.readValue(jsonString, pojoClass)
+
+			if (pojo != null)
+				out.add(pojo)
+
 		} catch (e: Exception) {
 			e.printStackTrace()
 			//TODO LOG KILL PUNISH
