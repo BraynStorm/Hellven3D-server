42fc164c866b4a500db8ea7edb73e98d6c0d4764
braynstorm <glav0r3zzz4@gmail.com>
2017-09-05 17:01:34 +0300

    Rearranged some stuff

--- src/main/kotlin/hellven3d/net/ExternalPackets.kt
+++ src/main/kotlin/hellven3d/net/ExternalPackets.kt
@@ -22,19 +22,24 @@ data class AuthFinished(
 }
 
 
+data class RequestCharacterList(
+		@JsonProperty("worldID") val worldID: Int
+) : ExternalPOJO
+
+data class WorldList(
+		val worlds: List<WorldInfo>
+) : ExternalPOJO
+
+
 class RequestWorldList : ExternalPOJO {
+
 	override fun toString(): String {
 		return javaClass.simpleName + "()"
 	}
 }
 
-data class RequestCharacterList(
-		@JsonProperty("worldID") val worldID: Int
-) : ExternalPOJO
-
-
-data class WorldList(
-		val worlds: List<WorldInfo>
+data class CharacterList(
+		val list: List<CharacterPrototype>
 ) : ExternalPOJO
 
 data class WorldInfo(
@@ -44,23 +49,19 @@ data class WorldInfo(
 		val current: Int,
 		val yourCharacters: Int,
 		val connectionString: String
-) : ExternalPOJO
-
-data class CharacterList(
-		val list: List<CharacterPrototype>
-) : ExternalPOJO
+)
 
 data class CharacterPrototype(
 		val id: Int,
 		val name: String,
 		val race: Int,
 		val equipment: MutableMap<Int, Item>
-) : ExternalPOJO
+)
 
 data class Item(
 		val id: Int,
 		val type: Short,
 		val subtype: Short
-) : ExternalPOJO {
+) {
 	constructor(item: hellven3d.server.Item) : this(item.id, item.type, item.subtype)
 }
